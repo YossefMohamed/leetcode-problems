ddbb118db051ed8b602f9d6741fd2725b2dfe45f
Yossef Mohamed <47724276+YossefMohamed@users.noreply.github.com>
2024-04-27 15:14:27 +0300

    Time: 90 ms (13.86%) | Memory: 54.1 MB (13.14%) - LeetSync

--- 7-reverse-integer/reverse-integer.ts
+++ 7-reverse-integer/reverse-integer.ts
@@ -1,19 +1,15 @@
 function reverse(x: number): number {
-    const maxInt = Math.pow(2, 31) - 1;
-    const minInt = Math.pow(-2, 31);
-    
-    let reversed = 0;
-    while (x !== 0) {
-        const digit = x % 10;
-        x = Math.trunc(x / 10);
-        if (reversed > maxInt / 10 || (reversed === maxInt / 10 && digit > 7)) {
-            return 0; // Overflow, return 0 as required
-        }
-        if (reversed < minInt / 10 || (reversed === minInt / 10 && digit < -8)) {
-            return 0; // Overflow, return 0 as required
-        }
-        reversed = reversed * 10 + digit;
+      const maxInt = Math.pow(2, 31) - 1;
+
+      
+    let newNumber: string = '';
+    let numString = `${x}`;
+    for (let i = numString.length - 1; i >= 0; i--) {
+        newNumber += numString[i];
     }
-    
-    return reversed;
+
+     let reversedNumber=parseInt(newNumber, 10)
+     if(Math.abs(reversedNumber) > maxInt  ) return 0
+    if(x<0) reversedNumber*=-1
+    return reversedNumber;
 }
