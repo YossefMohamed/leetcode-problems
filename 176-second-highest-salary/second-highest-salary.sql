dab04b9b321e7a1f0b00473ee130322681ffed97
Youssef Mohamed <47724276+YossefMohamed@users.noreply.github.com>
2024-06-01 11:06:27 +0300

    Time: 426 ms (69.12%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,7 @@
 SELECT 
-    MAX(salary) AS SecondHighestSalary
-FROM 
-    Employee
-WHERE 
-    salary < (SELECT MAX(salary) FROM Employee);
+    COALESCE(
+        (SELECT DISTINCT salary 
+         FROM Employee 
+         ORDER BY salary DESC 
+         LIMIT 1 OFFSET 1), 
+    NULL) AS SecondHighestSalary;
