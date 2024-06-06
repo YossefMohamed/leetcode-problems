93146b95d10e7f7150d3f5818bacaa4346c77b31
Youssef Mohamed <47724276+YossefMohamed@users.noreply.github.com>
2024-06-06 20:14:24 +0300

    Time: 41 ms (99.04%) | Memory: 51.7 MB (68.27%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.ts
@@ -0,0 +1,24 @@
+const getGreatestNumber = (array: number[]): number => {
+    let greatest = 0;
+    array.forEach(number => greatest = number > greatest ? number : greatest);
+    return greatest;
+}
+
+function lastStoneWeight(stones: number[]): number {
+    while (stones.length > 1) {
+        const greatest1 = getGreatestNumber(stones);
+        const index1 = stones.indexOf(greatest1);
+        stones.splice(index1, 1);
+
+        const greatest2 = getGreatestNumber(stones);
+        const index2 = stones.indexOf(greatest2);
+        stones.splice(index2, 1);
+
+        const smashResult = greatest1 - greatest2;
+        if (smashResult !== 0) {
+            stones.push(smashResult);
+        }
+    }
+    
+    return stones.length === 1 ? stones[0] : 0;
+}
